5cd4f3742767c501f8a8995034e250f91e321873
Jacob-Hocevar <jacob.hocevar@tamu.edu>
2024-11-01 01:44:03 -0500

    Updated Load Cell Calibration Program

--- Elysium/Real_Teensy/Load_Cell_Calibration/Load_Cell_Calibration.ino
+++ Elysium/Real_Teensy/Load_Cell_Calibration/Load_Cell_Calibration.ino
@@ -1,105 +1,81 @@
 /*
--------------------------------------------------------------------
-Load Cell Calibration
--------------------------------------------------------------------
-Program for the Teensy to output readings from 1-3 load cells and 
-report the results for calibration/verification.
+ Example using the SparkFun HX711 breakout board with a scale
+ By: Nathan Seidle
+ SparkFun Electronics
+ Date: November 19th, 2014
+ License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+ 
+ This is the calibration sketch. Use it to determine the calibration_factor that the main example uses. It also
+ outputs the zero_factor useful for projects that have a permanent mass on the scale in between power cycles.
+ 
+ Setup your scale and start the sketch WITHOUT a weight on the scale
+ Once readings are displayed place the weight on the scale
+ Press +/- or a/z to adjust the calibration_factor until the output readings match the known weight
+ Use this calibration_factor on the example sketch
+ 
+ This example assumes pounds (lbs). If you prefer kilograms, change the Serial.print(" lbs"); line to kg. The
+ calibration factor will be significantly different but it will be linearly related to lbs (1 lbs = 0.453592 kg).
+ 
+ Your calibration factor may be very positive or very negative. It all depends on the setup of your scale system
+ and the direction the sensors deflect from zero state
+
+ This example code uses bogde's excellent library: https://github.com/bogde/HX711
+ bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+
+ Arduino pin 2 -> HX711 CLK
+ 3 -> DOUT
+ 5V -> VCC
+ GND -> GND
+ 
+ Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
+ 
+ The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+ 
 */
 
-/*
--------------------------------------------------------------------
-VARIABLES & USER INPUT
--------------------------------------------------------------------
-*/
+#include "HX711.h" //This library can be obtained here http://librarymanager/All#Avia_HX711
 
-// time variables
-long unsigned sensor_update_last = 0;
-long unsigned sensor_update_interval = 100000;    // sensor update interval (microsec)     <-- USER INPUT
+#define LOADCELL_DOUT_PIN  3
+#define LOADCELL_SCK_PIN  2
 
-// BAUD rate 
-const int BAUD = 115200;                   // serial com in bits per second     <-- USER INPUT
+HX711 scale;
 
-/*
-LOAD CELL SET UP
-----------------
-*/
+float calibration_factor = -7050; //-7050 worked for my 440lb max scale setup
 
-// import library
-#include "HX711.h"
-
-// define data and clock pins for each loadcell
-const int LC1_D_OUT_PIN = -1;           // <-- USER INPUT
-const int LC1_CLK_PIN   = -1;           // <-- USER INPUT
-const int LC2_D_OUT_PIN = -1;           // <-- USER INPUT
-const int LC2_CLK_PIN   = -1;           // <-- USER INPUT
-const int LC3_D_OUT_PIN = -1;           // <-- USER INPUT
-const int LC3_CLK_PIN   = -1;           // <-- USER INPUT
-
-// measurement set up
-float weight1;
-float weight2;
-float weight3;
-HX711 scale1, scale2, scale3;
-
-/*
--------------------------------------------------------------------
-SETUP LOOP
--------------------------------------------------------------------
-*/
 void setup() {
-  Serial.begin(BAUD);           // initializes serial communication at set baud rate
-  
-  /*
-  LOAD CELL SET UP
-  -----------------------
-  */
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
 
-  // load cell setup
-  scale1.begin(LC1_D_OUT_PIN, LC1_CLK_PIN);
-  scale2.begin(LC2_D_OUT_PIN, LC2_CLK_PIN);
-  scale3.begin(LC3_D_OUT_PIN, LC3_CLK_PIN);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale();
+  scale.tare();	//Reset the scale to 0
 
-  // scale and tare load cells
-  scale1.set_scale(2280.f);  // Set the scale factor for conversion to kilograms
-  scale1.tare();             // Reset the scale to zero
-
-  scale2.set_scale(2280.f); // Set the scale factor for conversion to kilograms
-  scale2.tare();            // Reset the scale to zero
-  
-  scale3.set_scale(2280.f); // Set the scale factor for conversion to kilograms
-  scale3.tare();            // Reset the scale to zero
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
 }
 
-/*
--------------------------------------------------------------------
-LOOP
--------------------------------------------------------------------
-*/
 void loop() {
-  // check for last reading update
-  if ((micros() - sensor_update_last) > sensor_update_interval) {
-    sensor_update_last = micros();                               // update last time update
 
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
-    // measure force from each load cell
-    weight1 = scale1.get_units(1); // Get the weight in kilograms
-    weight2 = scale2.get_units(1); // Get the weight in kilograms
-    weight3 = scale3.get_units(1); // Get the weight in kilograms
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1);
+  Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
 
-    // send data to serial monitor 
-    Serial.print("t:"); Serial.print(sensor_update_last);                                  // print time reading
-    
-    // If the load cell has been defined, print the output.
-    // Values printed in kg
-    if (LC1_D_OUT_PIN >= 0) {
-      Serial.print(",L1:"); Serial.print(weight1);
-    } if (LC2_D_OUT_PIN >= 0) {
-      Serial.print(",L2:"); Serial.print(weight2);
-    } if (LC3_D_OUT_PIN >= 0) {
-      Serial.print(",L3:"); Serial.print(weight3);
-    }
-    Serial.println();
-    delay(10);
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
   }
-
 }
